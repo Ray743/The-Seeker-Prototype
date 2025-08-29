bf107500f10d9196d6366ea92b04e955eaffecb9
Raynold Bobola <62842393+Ray743@users.noreply.github.com>
2025-08-29 06:16:45 -0700

    Update test.py

--- seeker_back-end/test.py
+++ seeker_back-end/test.py
@@ -1,12 +1,1 @@
-import mysql.connector
 
-try:
-    db = mysql.connector.connect(
-        host="194.233.80.19",
-        user="seeker",
-        password="s33k3r01",
-        database="job_postings"
-    )
-    print("Connected successfully!")
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
